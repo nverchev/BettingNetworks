031076dc9921f6157f98edb4b9bc2e549236d554
nverchev <nicolas.vercheval@gmail.com>
2022-06-10 13:31:36 +0200

    new minio routine

--- EfficientNet/main.py
+++ EfficientNet/main.py
@@ -1,6 +1,6 @@
 import argparse
 import torch
-from torch.utils.data import Dataset
+from minio import Minio
 from dataset import get_dataset
 from model import get_model
 from trainer import get_trainer
@@ -14,6 +14,7 @@ def parse_args():
     parser.add_argument('--model_name', default='BaselineClassifier',
                         choices=['BaselineClassifier', 'BettingNetworks', 'BettingNetworksTwoHeaded'])
     parser.add_argument('--loss', default='BCELoss', choices=["BCE", "MAE", "MSE", "Naive", "Betting", "CrossBet"])
+    parser.add_argument('--dir_path', type=str, default='./', help='Directory for storing data and models')
     parser.add_argument('--exp_name', type=str, default='', help='Name of the experiment.')
     parser.add_argument('--eval', type=bool, default=False,
                         help='evaluate the model (exp_name needs to start with "final")')
@@ -25,6 +26,9 @@ def parse_args():
                         , help='SGD has no momentum, otherwise momentum = 0.9')
     parser.add_argument('--wd', type=float, default=0., help='weight decay')
     parser.add_argument('--cuda', type=bool, default=True, help='enables CUDA training')
+    parser.add_argument('--minio_credential', type=str, default='',
+                        help='path of file with written server.access_key.secret_key')
+
     return parser.parse_args()
 
 
@@ -33,14 +37,23 @@ if __name__ == '__main__':
     loss_name = args.loss
     model_name = args.model_name
     model_eval = args.eval
+    dir_path = args.dir_path
     training_epochs = args.epochs
     batch_size = args.batch_size
     opt = args.optimizer
     learning_rate = args.lr
     weight_decay = args.wd
     experiment = args.experiment
+    minio_credential = args.minio_credential
+    if minio_credential:
+        with open(minio_credential) as f:
+            server, access_key, secret_key = f.readline().split('.')
+            minioClient = Minio(server, access_key=access_key, secret_key=secret_key, secure=True)
+    else:
+        minio_credential = None
+
     device = torch.device("cuda:0" if torch.cuda.is_available() and args.cuda else "cpu")
-    train_loader, val_loader, test_loader = get_dataset(experiment, batch_size)
+    train_loader, val_loader, test_loader = get_dataset(experiment, batch_size, dirpath="./")
     optimizer, optim_args = get_opt(model_name, opt, learning_rate, weight_decay)
     block_args = {
         'optim_name': opt,
@@ -52,6 +65,8 @@ if __name__ == '__main__':
         'device': device,
         'batch_size': batch_size,
         'schedule': CosineSchedule(),
+        'minioClient': minioClient,
+        'dir_path': dir_path
     }
     for k, v in block_args.items():
         if not isinstance(v, (type, torch.utils.data.dataloader.DataLoader)):
@@ -70,4 +85,3 @@ if __name__ == '__main__':
 
     if experiment[:5] == 'final':
         trainer.test(partition='test')
-
